73d4567661023336405abc7c80b278c7f930e1fb
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-29 01:30:10 +0300

    Provide functionality to SimpleUserList class.

--- src/simple_user/SimpleUserList.java
+++ src/simple_user/SimpleUserList.java
@@ -19,11 +19,41 @@ public class SimpleUserList {
 		return this.simpleUsers;
 	}
 	
-	public void registerSimpleUser(SimpleUser simpleUser) {
-		if (this.simpleUserDB.insertSimpleUser(simpleUser)) {
+	public boolean registerSimpleUser(SimpleUser simpleUser) {
+		if (this.simpleUserDB.insertSimpleUser(simpleUser) == 1) {
 			System.out.println("Registration Succeeded!");
+			return true;
 		} else {
 			System.out.println("Registration Failed!");
+			return false;
+		}
+	}
+	
+	public boolean updateSimpleUser(SimpleUser simpleUser) {
+		if (this.simpleUserDB.updateSimpleUser(simpleUser) == 1) {
+			System.out.println("Update Succeeded!");
+			return true;
+		} else {
+			System.out.println("Update Failed!");
+			return false;
+		}
+	}
+	
+	public boolean isRegistered(int simpleUserID) {
+		return (this.findSimpleUser(simpleUserID) != null) ? true : false;
+	}
+	
+	public SimpleUser findSimpleUser(int simpleUserID) {
+		return this.simpleUserDB.selectSimpleUser(simpleUserID);
+	}
+	
+	public boolean unregisterSimpleUser(SimpleUser simpleUser) {
+		if (this.simpleUserDB.deleteSimpleUser(simpleUser) == 1) {
+			System.out.println("Deletion Succeeded!");
+			return true;
+		} else {
+			System.out.println("Deletion Failed!");
+			return false;
 		}
 	}
 	
@@ -31,7 +61,27 @@ public class SimpleUserList {
 		return this.simpleUserDB.getHighestID() + 1;
 	}
 	
+	public int getNumberOfSimpleUsers() {
+		return this.simpleUserDB.countSimpleUsers();
+	}
+	
+	public boolean checkByLocation(String location) {
+		return !this.getSimpleUsers(location).isEmpty();
+	}
+	
+	public ArrayList<SimpleUser> getSimpleUsers(String location) {
+		return this.simpleUserDB.selectSimpleUsers(location);
+	}
+	
+	public boolean checkByName(String firstName, String lastName) {
+		return !this.getSimpleUsers(firstName,lastName).isEmpty();
+	}
+	
+	public ArrayList<SimpleUser> getSimpleUsers(String firstName, String lastName) {
+		return this.simpleUserDB.selectSimpleUsers(firstName, lastName);
+	}
+	
 	public ArrayList<SimpleUser> getAllSimpleUsers() {
-		return this.simpleUserDB.getAllSimpleUsers();
+		return this.simpleUserDB.selectAllSimpleUsers();
 	}
 }
--- src/testers/SimpleUserListTester.java
+++ src/testers/SimpleUserListTester.java
@@ -9,18 +9,67 @@ public class SimpleUserListTester {
 
 	public static void main(String[] args) {
 		SimpleUserList simpleUserList = new SimpleUserList();
-		
+
 		// Register a new SimpleUser
-		simpleUserList.registerSimpleUser(new SimpleUser(
-				simpleUserList.getNextSimpleUserID(),
-				"George",
-				"Spyridakis",
-				"Athens"));
-		
+		simpleUserList.registerSimpleUser(
+				new SimpleUser(simpleUserList.getNextSimpleUserID(), "George", "Spyridakis", "Marousi"));
+		System.out.println("\n");
+
+		// Update SimpleUser
+		simpleUserList.updateSimpleUser(new SimpleUser(4, "George", "Spyridakis", "Marousi"));
+		System.out.println("\n");
+
+		// Delete SimpleUser
+		simpleUserList.unregisterSimpleUser(
+				new SimpleUser(simpleUserList.getNextSimpleUserID(), "George", "Spyridakis", "Marousi"));
+		System.out.println("\n");
+		simpleUserList.unregisterSimpleUser(new SimpleUser(4, "George", "Spyridakis", "Marousi"));
+		System.out.println("\n");
+		simpleUserList.unregisterSimpleUser(new SimpleUser(25, "", "", ""));
+		System.out.println("\n");
+
+		// Find SimpleUser by ID
+		System.out.println(simpleUserList.findSimpleUser(2).toString() + " found\n");
+
+		// Check if SimpleUser is registered
+		System.out.println("ID 300 is registered: " + simpleUserList.isRegistered(300) + "\n");
+		System.out.println("ID 2 is registered: " + simpleUserList.isRegistered(2) + "\n");
+
+		// Get SimpleUsers by location
+		System.out.println("Marousi: " + simpleUserList.checkByLocation("Marousi"));
+		ArrayList<SimpleUser> simpleUsersByLocation = simpleUserList.getSimpleUsers("Marousi");
+		for (SimpleUser simpleUser : simpleUsersByLocation) {
+			System.out.println(simpleUser.toString());
+		}
+		System.out.println();
+		System.out.println("Marousiaaaaa: " + simpleUserList.checkByLocation("Marousiaaaaa"));
+		simpleUsersByLocation = simpleUserList.getSimpleUsers("Marousiaaaaa");
+		for (SimpleUser simpleUser : simpleUsersByLocation) {
+			System.out.println(simpleUser.toString());
+		}
+		System.out.println();
+
+		// Get SimpleUsers by name
+		System.out.println("George Spyridakis exists: " + simpleUserList.checkByName("George", "Spyridakis"));
+		ArrayList<SimpleUser> simpleUsersByName = simpleUserList.getSimpleUsers("George", "Spyridakis");
+		for (SimpleUser simpleUser : simpleUsersByName) {
+			System.out.println(simpleUser.toString());
+		}
+		System.out.println();
+		System.out.println("George Spydakis exists: " + simpleUserList.checkByName("George", "Spydakis"));
+		simpleUsersByName = simpleUserList.getSimpleUsers("George", "Spydakis");
+		for (SimpleUser simpleUser : simpleUsersByName) {
+			System.out.println(simpleUser.toString());
+		}
+		System.out.println();
+
 		// Get all SimpleUsers
 		ArrayList<SimpleUser> simpleUsers = simpleUserList.getAllSimpleUsers();
 		for (SimpleUser simpleUser : simpleUsers) {
 			System.out.println(simpleUser.toString());
 		}
+
+		// Get total number of Registered SimpleUsers
+		System.out.println("\nNumber of SimpleUsers: " + simpleUserList.getNumberOfSimpleUsers());
 	}
 }
