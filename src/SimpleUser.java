f2805a5ab51f296e68c2a3291a0a168838ab5e0f
George Spiridakis <spiridakis.grg@gmail.com>
2016-10-28 14:47:44 +0300

    Create the SimpleUser class.

---
+++ src/SimpleUser.java
@@ -0,0 +1,49 @@
+
+public class SimpleUser {
+
+	private final int simpleUserID;
+	private String firstName;
+	private String lastName;
+	private String location;
+	public static int counterID = 0;
+
+	public SimpleUser() {
+		counterID++;
+		this.simpleUserID = counterID;
+	}
+
+	public SimpleUser(String firstName, String lastName, String location) {
+		this();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.location = location;
+	}
+
+	public int getSimpleUserID() {
+		return this.simpleUserID;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getLocation() {
+		return this.location;
+	}
+}
