559a6a83cb3dbce5b25ee4273b2f2c936cf0a05d
Dimitris-Theodoropoulos <Theodoropoulos.Dimitris2@gmail.com>
2016-10-30 15:57:57 +0200

    Create the DaoUtils class in dao package.

---
+++ src/dao/DaoUtils.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Utility class used for retrieving database connection
+ * 
+ * @author Dimitris
+ *
+ */
+public class DaoUtils {
+	private static final String databaseUrl = "jdbc:mysql://localhost/bids4job_db?useSSL=false";
+	private static final String databaseUsername = "root";
+	private static final String databasePassword = "xxxxx";
+
+	public static Connection getConnection()
+			throws SQLException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+		if (!DriverManager.getDrivers().hasMoreElements()) {
+			registerDriver();
+		}
+		return DriverManager.getConnection(databaseUrl, databaseUsername, databasePassword);
+	}
+
+	public static void registerDriver()
+			throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+		DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
+	}
+}
