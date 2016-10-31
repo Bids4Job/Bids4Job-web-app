60c3478a132c3859c17e8baa29509a2a6071f8be
Dimitris-Theodoropoulos <Theodoropoulos.Dimitris2@gmail.com>
2016-10-31 23:01:43 +0200

    Update the TaskTester class.

--- src/testers/TaskTester.java
+++ src/testers/TaskTester.java
@@ -1,14 +1,32 @@
 package testers;
 
-import com.sun.jmx.snmp.Timestamp;
+import java.sql.Timestamp;
 
-import dao.TaskDao;
 import domain.Task;
 
 public class TaskTester {
 
 	public static void main(String[] args) {
-		Task task1 = new Task();
-		
+		/**
+		 * Low dependency from constructor.
+		 */
+		Task task = new Task().setDeadline(Timestamp.valueOf("2019-11-12 00:00:00")).setSimpleUserId(5)
+				.setWorkField("musician").setTaskId(20);
+		System.out.println(task);
+		/**
+		 * Testing the getters.
+		 */
+		System.out.println(task.getSimpleUserId());
+		System.out.println(task.getTaskId());
+		System.out.println(task.getWorkField());
+		System.out.println(task.getDeadline());
+		/**
+		 * Testing the setters and toString() method.
+		 */
+		task.setDeadline(Timestamp.valueOf("2020-12-12 00:00:00"));
+		task.setSimpleUserId(32);
+		task.setTaskId(55);
+		task.setWorkField("artist");
+		System.out.println(task.toString());
 	}
 }
