1a6cac4eaa7a4e13b443ebbe2411cd63364d139a
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-05 12:22:28 +0200

    Provide sql scripts for generating dummy data in db.

---
+++ db_setup/contract.sql
@@ -0,0 +1,7 @@
+INSERT INTO `bids4job_db`.`contract` (`contract_id`, `bid_id`, `task_id`, `contract_time`) VALUES ('1', '3', '1', '2016-01-05 15:30:00');
+INSERT INTO `bids4job_db`.`contract` (`contract_id`, `bid_id`, `task_id`, `contract_time`) VALUES ('2', '10', '3', '2016-01-09 22:00:00');
+INSERT INTO `bids4job_db`.`contract` (`contract_id`, `bid_id`, `task_id`, `contract_time`) VALUES ('3', '17', '15', '2016-02-20 17:30:00');
+INSERT INTO `bids4job_db`.`contract` (`contract_id`, `bid_id`, `task_id`, `contract_time`) VALUES ('4', '31', '17', '2016-02-11 16:00:00');
+INSERT INTO `bids4job_db`.`contract` (`contract_id`, `bid_id`, `task_id`, `contract_time`) VALUES ('5', '33', '73', '2017-01-02 10:00:00');
+INSERT INTO `bids4job_db`.`contract` (`contract_id`, `bid_id`, `task_id`, `contract_time`) VALUES ('6', '41', '79', '2017-01-08 08:00:00');
+INSERT INTO `bids4job_db`.`contract` (`contract_id`, `bid_id`, `task_id`, `contract_time`) VALUES ('7', '51', '77', '2017-01-03 23:30:00');
