f6f2dc996ad9e31ea8eb77f92dac82b147203d5b
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-07 00:30:58 +0200

    Display the successfully created SimpleUser.

--- src/servlet/CreateSimpleUserServlet.java
+++ src/servlet/CreateSimpleUserServlet.java
@@ -2,12 +2,14 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import domain.SimpleUser;
 import service.SimpleUserService;
 
 /**
@@ -18,8 +20,8 @@ public class CreateSimpleUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	// Parameter names
-	private static final String NAME = "name";
-	private static final String SURNAME = "surname";
+	private static final String FIRST_NAME = "firstName";
+	private static final String LAST_NAME = "lastName";
 	private static final String EMAIL = "email";
 	private static final String USERNAME = "username";
 	private static final String PASSWORD = "password";
@@ -49,37 +51,42 @@ public class CreateSimpleUserServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// RequestDispatcher to forward in created and stored successfully in database
+		RequestDispatcher successDispatcher = getServletContext().getRequestDispatcher("/create_success.jsp");
+		
 		// Instantiate a service for SimpleUser database operations
 		SimpleUserService simpleUserService = new SimpleUserService();
 
 		// Prepare an error message & error counter
 		String errorMessage = "";
-		
+
 		// Get parameters from the request
-		String name = request.getParameter(NAME);
-		String surname = request.getParameter(SURNAME);
+		String firstName = request.getParameter(FIRST_NAME);
+		String lastName = request.getParameter(LAST_NAME);
 		String email = request.getParameter(EMAIL);
 		String username = request.getParameter(USERNAME);
 		String password = request.getParameter(PASSWORD);
 		String location = request.getParameter(LOCATION);
 
-		errorMessage = checkNullity(name, surname, email, username, password, location);
-		if (!errorMessage.equals("")) {
+		errorMessage = checkNullity(firstName, lastName, email, username, password, location);
+		if (errorMessage.equals("")) {
 			response.getWriter().append(errorMessage);
 		}
 
-		response.getWriter()
-				.append(name + "\n" + surname + "\n" + email + "\n" + username + "\n" + password + "\n" + location);
+		SimpleUser simpleUser = new SimpleUser().setFirstName(firstName).setLastName(lastName).setLocation(location);
+		// Set SimpleUser to request
+		request.setAttribute("simpleUser", simpleUser);
+		successDispatcher.forward(request, response);
 	}
 
 	private String checkNullity(String name, String surname, String email, String username, String password,
 			String location) {
 		StringBuilder errorBuilder = new StringBuilder();
 		if (name == null || name.length() == 0) {
-			errorBuilder.append(NAME).append(" field is empty").append("\n");
+			errorBuilder.append(FIRST_NAME).append(" field is empty").append("\n");
 		}
 		if (surname == null || surname.length() == 0) {
-			errorBuilder.append(SURNAME).append(" field is empty").append("\n");
+			errorBuilder.append(LAST_NAME).append(" field is empty").append("\n");
 		}
 		if (email == null || email.length() == 0) {
 			errorBuilder.append(EMAIL).append(" field is empty").append("\n");
