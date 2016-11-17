73c474a15d8c1965608c8722d8053c594f1a4cbf
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-17 13:03:53 +0200

    Add the "proRating" field in Contract.

--- src/main/java/domain/Contract.java
+++ src/main/java/domain/Contract.java
@@ -8,56 +8,69 @@ public class Contract {
 	private int contractID;
 	private int bidID;
 	private int taskID;
+	private double proRating;
 	private Timestamp contractTime;
-	
-	public Contract(){}
-	
-	public Contract(int contractID, int taskID, int bidID, Timestamp contractTime) {
+
+	public Contract() {
+	}
+
+	public Contract(int contractID, int taskID, int bidID, double proRating, Timestamp contractTime) {
 		this.contractID = contractID;
 		this.taskID = taskID;
 		this.bidID = bidID;
+		this.proRating = proRating;
 		this.contractTime = contractTime;
 	}
-	
+
 	public Contract setContractID(int contractID) {
 		this.contractID = contractID;
 		return this;
 	}
-	
+
 	public int getContractID() {
 		return this.contractID;
 	}
-	
+
 	public Contract setBidID(int bidID) {
 		this.bidID = bidID;
 		return this;
 	}
-	
+
 	public int getBidID() {
 		return this.bidID;
 	}
-	
+
 	public Contract setTaskID(int taskID) {
 		this.taskID = taskID;
 		return this;
 	}
-	
+
 	public int getTaskID() {
 		return this.taskID;
 	}
-	
+
+	public Contract setProRating(double rating) {
+		this.proRating = rating;
+		return this;
+	}
+
+	public double getProRating() {
+		return this.proRating;
+	}
+
 	public Contract setContractTime(Timestamp contractTime) {
 		this.contractTime = contractTime;
 		return this;
 	}
-	
+
 	public Timestamp getContractTime() {
 		return this.contractTime;
 	}
-	
+
 	@Override
 	public String toString() {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		return "Contract " + this.contractID + ": task " + this.taskID + ", bid " + this.bidID + ", contract time " + simpleDateFormat.format(this.contractTime);
+		return "Contract " + this.contractID + ": task " + this.taskID + ", bid " + this.bidID + ", pro rating "
+				+ this.proRating + ", contract time " + simpleDateFormat.format(this.contractTime);
 	}
 }
--- src/main/java/testers/ContractTester.java
+++ src/main/java/testers/ContractTester.java
@@ -8,14 +8,15 @@ public class ContractTester {
 
 	public static void main(String[] args) {
 		// Contract 1
-		Contract contract1 = new Contract(1, 5, 18, new Timestamp(System.currentTimeMillis()));
+		Contract contract1 = new Contract(1, 5, 18, 4.3, new Timestamp(System.currentTimeMillis()));
 		System.out.println(contract1);
 		System.out.println("Contract ID: " + contract1.getContractID());
 		System.out.println("Bid ID: " + contract1.getBidID());
 		System.out.println("Task ID: " + contract1.getTaskID());
+		System.out.println("Pro Rating: " + contract1.getProRating());
 		System.out.println("ContractTime: " + contract1.getContractTime());
 		// Contract 2
-		Contract contract2 = new Contract().setTaskID(20).setBidID(13).setContractID(10)
+		Contract contract2 = new Contract().setTaskID(20).setBidID(13).setContractID(10).setProRating(4.2)
 				.setContractTime(new Timestamp(System.currentTimeMillis()));
 		System.out.println(contract2);
 		System.out.println("ContractTime: " + contract2.getContractTime());
