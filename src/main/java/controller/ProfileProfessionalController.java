47a371e0be1b7e22705003be5a47010cac2d93d7
George Spiridakis <spiridakis.grg@gmail.com>
2016-11-19 11:56:38 +0200

    Make minor changes in profile pages.

--- src/main/java/controller/ProfileProfessionalController.java
+++ src/main/java/controller/ProfileProfessionalController.java
@@ -22,57 +22,58 @@ import service.TaskService;
 @WebServlet("/profile_professional")
 public class ProfileProfessionalController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private ContractService contractService;
 	private TaskService taskService;
-	
+
 	RequestDispatcher errorDispatcher;
 	RequestDispatcher profileDispatcher;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProfileProfessionalController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-    @Override
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ProfileProfessionalController() {
+		super();
+	}
+
+	@Override
 	public void init() {
 		// Define RequestDispatcher object to forward any errors
 		errorDispatcher = getServletContext().getRequestDispatcher("/errorprinter.jsp");
 		profileDispatcher = getServletContext().getRequestDispatcher("/prouserprofile.jsp");
 
-		// Instantiate a Contract service object
+		// Instantiate a Contract and a Task service objects
 		contractService = new ContractService();
 		taskService = new TaskService();
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	    	response.setContentType("text/html; charset=UTF-8");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
-		
+
 		// Get the HttpSession that is associated with this request
 		HttpSession session = request.getSession();
-		
+
 		int proUserId;
-		
+
 		try {
 			// Get Professional User ID
-		    	proUserId = ((ProfessionalUser) session.getAttribute("pro")).getProUserId();
+			proUserId = ((ProfessionalUser) session.getAttribute("pro")).getProUserId();
 		} catch (NullPointerException e) {
 			e.printStackTrace();
 			request.setAttribute("errorMessage", "Log in First! ");
 			errorDispatcher.forward(request, response);
 			return;
 		}
-		
+
 		CachedRowSet crsTasks = null;
-		
+
 		try {
 			crsTasks = taskService.findDetailsByProfessionalUserID(proUserId);
 		} catch (IllegalAccessException | InstantiationException | ClassNotFoundException | SQLException e) {
@@ -80,12 +81,12 @@ public class ProfileProfessionalController extends HttpServlet {
 			request.setAttribute("errorMessage", "Error Loading Tasks in Profile: " + e.getMessage());
 			errorDispatcher.forward(request, response);
 		}
-		
+
 		String rating = null;
-		
-		try {	
-		    	rating = contractService.findRatingByProUserID(((ProfessionalUser) session.getAttribute("pro")).getProUserId());
-			
+
+		try {
+			rating = contractService
+					.findRatingByProUserID(((ProfessionalUser) session.getAttribute("pro")).getProUserId());
 		} catch (IllegalAccessException | InstantiationException | ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 			request.setAttribute("errorMessage", "Error Loading Profile: " + e.getMessage());
@@ -96,16 +97,15 @@ public class ProfileProfessionalController extends HttpServlet {
 		request.setAttribute("tasks", crsTasks);
 		request.setAttribute("rating", rating);
 		profileDispatcher.forward(request, response);
-		
-		
-		
-	    
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
--- src/main/webapp/prouserprofile.jsp
+++ src/main/webapp/prouserprofile.jsp
@@ -3,7 +3,7 @@
 <%@ page import="domain.ProfessionalUser"%>
 <%@ page import="javax.sql.rowset.CachedRowSet"%>
 <%@ page import="java.text.SimpleDateFormat"%>
-<%@ page errorPage="error.jsp"%>
+
 
 <!DOCTYPE html>
 <html>
@@ -48,8 +48,7 @@
 						class="icon-bar"></span> <span class="icon-bar"></span> <span
 						class="icon-bar"></span>
 				</button>
-				<a href="index"> <img alt="Brand" src="images/logo.png">
-					<!--Brand logo image-->
+				<a href="index"> <img alt="Brand" src="images/logo.png"> <!--Brand logo image-->
 				</a>
 			</div>
 			<!-- end collapsed navbar-->
@@ -88,10 +87,9 @@
 							</div>
 							<!-- Getting Attrin=bute from session -->
 							<%
-							    ProfessionalUser pro = (ProfessionalUser) session.getAttribute("pro");
-							    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-							    //String rating = (String) session.getAttribute("rating");
-							    String rating = (String) request.getAttribute("rating");
+								ProfessionalUser pro = (ProfessionalUser) session.getAttribute("pro");
+								SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+								String rating = (String) request.getAttribute("rating");
 							%>
 							<div class=" col-md-9 col-lg-9 ">
 								<table class="table table-user-information">
@@ -146,13 +144,13 @@
 
 					<!-- Get the contact details from the request -->
 					<%
-					    CachedRowSet crsTasks = (CachedRowSet) request.getAttribute("tasks");
-					    if (crsTasks.isBeforeFirst()) {
+						CachedRowSet crsTasks = (CachedRowSet) request.getAttribute("tasks");
+						if (crsTasks.isBeforeFirst()) {
 							int prevTaskID = 0;
 							while (crsTasks.next()) {
-							    int taskID = crsTasks.getInt("task_id");
-							    if (taskID != prevTaskID) {
-								if (prevTaskID != 0) {
+								int taskID = crsTasks.getInt("task_id");
+								if (taskID != prevTaskID) {
+									if (prevTaskID != 0) {
 					%>
 					<!-- Same snippets: (244 - 274) or (157 - 188) -->
 					</tbody>
@@ -189,8 +187,8 @@
 
 
 		<%
-		    }
-					prevTaskID = taskID;
+			}
+						prevTaskID = taskID;
 		%>
 		<div class="panel panel-default">
 			<div class="panel-heading" role="tab" id="heading<%=taskID%>">
@@ -216,7 +214,7 @@
 						</thead>
 						<tbody>
 							<%
-							    } // End 	if (taskID != prevTaskID)
+								} // End 	if (taskID != prevTaskID)
 							%>
 
 							<tr>
@@ -226,7 +224,7 @@
 								<td><%=simpleDateFormat.format(crsTasks.getTimestamp("bid_time"))%></td>
 								<td>
 									<%
-									    if (crsTasks.getString("username").equals(pro.getUsername())) {
+										if (crsTasks.getString("username").equals(pro.getUsername())) {
 									%>
 									<form class="form-inline" method="POST" action="cancel_bid">
 										<input type="hidden" name="bidId"
@@ -235,12 +233,12 @@
 											<i class="glyphicon glyphicon-remove"></i>
 										</button>
 									</form> <%
-     }
+ 	}
  %>
 								</td>
 							</tr>
 							<%
-							    } // End 	while(crs.next())
+								} // End 	while(crs.next())
 							%>
 
 							<!-- Same snippets: (244 - 274) or (157 - 188) -->
@@ -276,7 +274,7 @@
 			<!-- End .panel-collapse collapse in -->
 
 			<%
-			    }
+				}
 			%>
 		</div>
 		<!-- End .panel panel-default -->
