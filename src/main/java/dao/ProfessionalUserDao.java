c4341253da8af432c2cc5e31c6ba03ce6fd2ffab
Ioannis Kranas <ioakranas@gmail.com>
2016-11-29 14:44:30 +0200

    Add Javadoc to ProfessionalUserDao

--- src/main/java/dao/ProfessionalUserDao.java
+++ src/main/java/dao/ProfessionalUserDao.java
@@ -4,14 +4,15 @@ import domain.ProfessionalUser;
 import java.sql.*;
 import java.util.*;
 
-
 /**
  * 
  * @author Ioannis
  *
  */
 public class ProfessionalUserDao {
-    
+
+    // Necessary fields to connect to DB, execute queries and store the result
+    // sets.
     public static final String PROFFESIONAL_USER_TABLE = "pro_user";
     public static final String PRO_USER_ID = "pro_user_ID";
     public static final String FIRST_NAME = "first_name";
@@ -23,11 +24,11 @@ public class ProfessionalUserDao {
     public static final String EMAIL = "email";
     public static final String ACTIVE = "active_account";
     public static final String PHOTO_NAME = "pro_photo";
-    
+
     private Connection connection;
     private PreparedStatement statement;
     private ResultSet resultSet;
-    
+
     /**
      * Sets the connection, the preparedStatement and the resultSet to null.
      */
@@ -36,10 +37,10 @@ public class ProfessionalUserDao {
 	this.statement = null;
 	this.resultSet = null;
     }
-    
-    
+
     /**
      * Update a specific Professional User
+     * 
      * @param professionalUser
      * @return
      * @throws SQLException
@@ -47,8 +48,11 @@ public class ProfessionalUserDao {
      * @throws ClassNotFoundException
      * @throws InstantiationException
      */
-    public boolean update (ProfessionalUser professionalUser) throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
-	String query = "UPDATE " + PROFFESIONAL_USER_TABLE + " SET " + FIRST_NAME +  "= ?, " + LAST_NAME + "= ?, " + LOCATION + "= ?, " + PROFESSION + "= ?, " + USERNAME + "= ?, " + PASSWORD + "= ?, " + EMAIL + "= ?, " + ACTIVE + "= ? WHERE " + PRO_USER_ID +  "=?";
+    public boolean update(ProfessionalUser professionalUser)
+	    throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+	String query = "UPDATE " + PROFFESIONAL_USER_TABLE + " SET " + FIRST_NAME + "= ?, " + LAST_NAME + "= ?, "
+		+ LOCATION + "= ?, " + PROFESSION + "= ?, " + USERNAME + "= ?, " + PASSWORD + "= ?, " + EMAIL + "= ?, "
+		+ ACTIVE + "= ? WHERE " + PRO_USER_ID + "=?";
 	this.prepareResources();
 	try {
 	    connection = DaoUtils.getConnection();
@@ -63,7 +67,7 @@ public class ProfessionalUserDao {
 	    statement.setBoolean(8, professionalUser.getActive());
 	    statement.setInt(9, professionalUser.getProUserId());
 	    int affectedRows = statement.executeUpdate();
-	    if (affectedRows == 1){
+	    if (affectedRows == 1) {
 		return true;
 	    }
 	} finally {
@@ -71,17 +75,18 @@ public class ProfessionalUserDao {
 	}
 	return false;
     }
-    
-    
+
     /**
      * Remove a specific Professional User
+     * 
      * @param proUserid
      * @throws SQLException
      * @throws IllegalAccessException
      * @throws ClassNotFoundException
      * @throws InstantiationException
      */
-    public boolean remove(ProfessionalUser professionalUser) throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public boolean remove(ProfessionalUser professionalUser)
+	    throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
 	String query = "DELETE FROM " + PROFFESIONAL_USER_TABLE + " WHERE " + PRO_USER_ID + " = ?";
 	this.prepareResources();
 	try {
@@ -89,7 +94,7 @@ public class ProfessionalUserDao {
 	    statement = connection.prepareStatement(query);
 	    statement.setInt(1, professionalUser.getProUserId());
 	    int affectedRows = statement.executeUpdate();
-	    if (affectedRows == 1){
+	    if (affectedRows == 1) {
 		return true;
 	    }
 	} finally {
@@ -97,10 +102,10 @@ public class ProfessionalUserDao {
 	}
 	return false;
     }
-    
-            
+
     /**
      * Add a new Professional User in the bids4job_DB
+     * 
      * @param professionalUser
      * @return
      * @throws SQLException
@@ -108,9 +113,13 @@ public class ProfessionalUserDao {
      * @throws ClassNotFoundException
      * @throws InstantiationException
      */
-    public ProfessionalUser create(ProfessionalUser professionalUser) throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
-	String query = "INSERT INTO " + PROFFESIONAL_USER_TABLE + "(" + FIRST_NAME + ", " + LAST_NAME + ", " + LOCATION + ", " + PROFESSION + ", " + USERNAME + ", " + PASSWORD + ", " + EMAIL + ", " + ACTIVE + ", " + PHOTO_NAME + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	this.prepareResources();;
+    public ProfessionalUser create(ProfessionalUser professionalUser)
+	    throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+	String query = "INSERT INTO " + PROFFESIONAL_USER_TABLE + "(" + FIRST_NAME + ", " + LAST_NAME + ", " + LOCATION
+		+ ", " + PROFESSION + ", " + USERNAME + ", " + PASSWORD + ", " + EMAIL + ", " + ACTIVE + ", "
+		+ PHOTO_NAME + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	this.prepareResources();
+	;
 	try {
 	    connection = DaoUtils.getConnection();
 	    statement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
@@ -133,10 +142,10 @@ public class ProfessionalUserDao {
 	}
 	return professionalUser;
     }
-    
-    
+
     /**
      * Find a specific Professional User from location
+     * 
      * @param location
      * @return
      * @throws SQLException
@@ -144,7 +153,8 @@ public class ProfessionalUserDao {
      * @throws ClassNotFoundException
      * @throws InstantiationException
      */
-    public List<ProfessionalUser> findFromLocation(String location) throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public List<ProfessionalUser> findFromLocation(String location)
+	    throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
 	List<ProfessionalUser> professionalUsers = new ArrayList<>();
 	String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE + " WHERE " + LOCATION + " = ?";
 	this.prepareResources();
@@ -153,7 +163,7 @@ public class ProfessionalUserDao {
 	    statement = connection.prepareStatement(query);
 	    statement.setString(1, location);
 	    resultSet = statement.executeQuery();
-	    while (resultSet.next()){
+	    while (resultSet.next()) {
 		ProfessionalUser professionalUser = populate(resultSet);
 		professionalUsers.add(professionalUser);
 	    }
@@ -162,9 +172,10 @@ public class ProfessionalUserDao {
 	}
 	return professionalUsers;
     }
-    
+
     /**
      * Find all active Professional Users
+     * 
      * @param active
      * @return
      * @throws SQLException
@@ -172,7 +183,8 @@ public class ProfessionalUserDao {
      * @throws ClassNotFoundException
      * @throws InstantiationException
      */
-    public List<ProfessionalUser> findActive(boolean active)  throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public List<ProfessionalUser> findActive(boolean active)
+	    throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
 	List<ProfessionalUser> activeProfessionalUsers = new ArrayList<>();
 	String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE + " WHERE " + ACTIVE + " = ?";
 	this.prepareResources();
@@ -181,7 +193,7 @@ public class ProfessionalUserDao {
 	    statement = connection.prepareStatement(query);
 	    statement.setBoolean(1, active);
 	    resultSet = statement.executeQuery();
-	    while (resultSet.next()){
+	    while (resultSet.next()) {
 		ProfessionalUser professionalUser = populate(resultSet);
 		activeProfessionalUsers.add(professionalUser);
 	    }
@@ -189,12 +201,12 @@ public class ProfessionalUserDao {
 	    DaoUtils.closeResources(resultSet, statement, connection);
 	}
 	return activeProfessionalUsers;
-	
+
     }
-    
-    
+
     /**
      * Find specific Professional User from Professional User ID
+     * 
      * @param proUserid
      * @return
      * @throws SQLException
@@ -202,7 +214,8 @@ public class ProfessionalUserDao {
      * @throws ClassNotFoundException
      * @throws InstantiationException
      */
-    public ProfessionalUser findOne(int proUserid) throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    public ProfessionalUser findOne(int proUserid)
+	    throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
 	ProfessionalUser professionalUser = null;
 	String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE + " WHERE " + PRO_USER_ID + " = ?";
 	this.prepareResources();
@@ -211,7 +224,7 @@ public class ProfessionalUserDao {
 	    statement = connection.prepareStatement(query);
 	    statement.setInt(1, proUserid);
 	    resultSet = statement.executeQuery();
-	    if (resultSet.next()){
+	    if (resultSet.next()) {
 		professionalUser = populate(resultSet);
 	    }
 	} finally {
@@ -219,37 +232,38 @@ public class ProfessionalUserDao {
 	}
 	return professionalUser;
     }
-    
-    
-    
+
     /**
      * Find all Professional Users in the bids4job_DB
+     * 
      * @return
      * @throws SQLException
      * @throws IllegalAccessException
      * @throws ClassNotFoundException
      * @throws InstantiationException
      */
-    public List<ProfessionalUser> fiandAll() throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
-	 List<ProfessionalUser> professionalUsers = new ArrayList<>();
-	 String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE;
-	 this.prepareResources();
-	 try {
-	     connection = DaoUtils.getConnection();
-	     statement = connection.prepareStatement(query);
-	     resultSet = statement.executeQuery();
-	     while (resultSet.next()) {
-		 ProfessionalUser professionalUser = populate(resultSet);
-		 professionalUsers.add(professionalUser);
-	     }
-	 } finally {
-	     DaoUtils.closeResources(resultSet, statement, connection);
-	 }
-	 return professionalUsers;
+    public List<ProfessionalUser> fiandAll()
+	    throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+	List<ProfessionalUser> professionalUsers = new ArrayList<>();
+	String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE;
+	this.prepareResources();
+	try {
+	    connection = DaoUtils.getConnection();
+	    statement = connection.prepareStatement(query);
+	    resultSet = statement.executeQuery();
+	    while (resultSet.next()) {
+		ProfessionalUser professionalUser = populate(resultSet);
+		professionalUsers.add(professionalUser);
+	    }
+	} finally {
+	    DaoUtils.closeResources(resultSet, statement, connection);
+	}
+	return professionalUsers;
     }
-    
-    public ProfessionalUser findByEmail(String email) throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
-	
+
+    public ProfessionalUser findByEmail(String email)
+	    throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
+
 	ProfessionalUser pro = null;
 	String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE + " WHERE " + EMAIL + " =?;";
 	this.prepareResources();
@@ -261,16 +275,16 @@ public class ProfessionalUserDao {
 	    if (resultSet.next()) {
 		pro = populate(resultSet);
 	    }
-	    
+
 	} finally {
-		DaoUtils.closeResources(resultSet, statement, connection);
+	    DaoUtils.closeResources(resultSet, statement, connection);
 	}
 	return pro;
     }
-    
-    
-    public ProfessionalUser findByUsername(String username) throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
-	
+
+    public ProfessionalUser findByUsername(String username)
+	    throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
+
 	ProfessionalUser pro = null;
 	String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE + " WHERE " + USERNAME + " =?;";
 	this.prepareResources();
@@ -282,16 +296,16 @@ public class ProfessionalUserDao {
 	    if (resultSet.next()) {
 		pro = populate(resultSet);
 	    }
-	    
+
 	} finally {
-		DaoUtils.closeResources(resultSet, statement, connection);
+	    DaoUtils.closeResources(resultSet, statement, connection);
 	}
 	return pro;
     }
-    
-    
-    public boolean exist(String name, String value) throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
-	
+
+    public boolean exist(String name, String value)
+	    throws IllegalAccessException, InstantiationException, ClassNotFoundException, SQLException {
+
 	String query = "SELECT * FROM " + PROFFESIONAL_USER_TABLE + " WHERE " + name + " =?;";
 	this.prepareResources();
 	try {
@@ -304,32 +318,28 @@ public class ProfessionalUserDao {
 	    } else {
 		return false;
 	    }
-	    
+
 	} finally {
-		DaoUtils.closeResources(resultSet, statement, connection);
+	    DaoUtils.closeResources(resultSet, statement, connection);
 	}
-	
+
     }
-        
-              
+
     /**
-     * Utility method that takes a result set and returns a ProfessionalUser object.
+     * Utility method that takes a result set and returns a ProfessionalUser
+     * object.
+     * 
      * @param resultSet
      * @return
      * @throws SQLException
      */
     private ProfessionalUser populate(ResultSet resultSet) throws SQLException {
-	return new ProfessionalUser()
-		.setProUSerId(resultSet.getInt(PRO_USER_ID))
-		.setFirstName(resultSet.getString(FIRST_NAME))
-		.setLastName(resultSet.getString(LAST_NAME))
-		.setLocation(resultSet.getString(LOCATION))
-		.setProfession(resultSet.getString(PROFESSION))
-		.setUsername(resultSet.getString(USERNAME))
-		.setPassword(resultSet.getString(PASSWORD))
-		.setEmail(resultSet.getString(EMAIL))
-		.setActive(resultSet.getBoolean(ACTIVE))
+	return new ProfessionalUser().setProUSerId(resultSet.getInt(PRO_USER_ID))
+		.setFirstName(resultSet.getString(FIRST_NAME)).setLastName(resultSet.getString(LAST_NAME))
+		.setLocation(resultSet.getString(LOCATION)).setProfession(resultSet.getString(PROFESSION))
+		.setUsername(resultSet.getString(USERNAME)).setPassword(resultSet.getString(PASSWORD))
+		.setEmail(resultSet.getString(EMAIL)).setActive(resultSet.getBoolean(ACTIVE))
 		.setPhotoName(resultSet.getString(PHOTO_NAME));
-    }   
-    
+    }
+
 }
